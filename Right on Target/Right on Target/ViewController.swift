38f3690e771d20294775ce0102af4efa550f7425
Georgiy <33875528+georg1856@users.noreply.github.com>
2022-09-06 17:02:30 +1000

    Create logic

--- Right on Target/Right on Target/ViewController.swift
+++ Right on Target/Right on Target/ViewController.swift
@@ -8,12 +8,55 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // загаданное число
+    var number = 0
+    
+    // раунд
+    var round = 0
+    
+    // сумма очков за раунд
+    var points = 0
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func checkNumber(_ sender: Any) {
+        
+        if round == 0 {
+            number = Int.random(in: 1...50)
+            label.text = String(number)
+            round = 1
+        } else {
+            let numSlider = Int(slider.value.rounded())
+            
+            if numSlider > number {
+                self.points += 50 - numSlider + self.number
+            } else if numSlider < self.number {
+                    self.points += 50 - self.number + numSlider
+                } else {
+                    self.points += 50 }
+            
+            if self.round == 5 {
+                let alert = UIAlertController(title: "Игра окончена",
+                                              message: "Вы заработали \(self.points) очков",
+                                              preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Начать заново",
+                                              style: .default,
+                                              handler: nil))
+                self.present(alert, animated: true, completion: nil)
+                self.round = 1
+                self.points = 0
+            } else {
+                self.round += 1
+            }
+            
+            self.number = Int.random(in: 1...50)
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
